01432a94683cc4d98e05fe4892fac6dd0869ea5f
Kateryna Hrinchak <92380648+KatyaHrinchak@users.noreply.github.com>
2026-02-05 23:10:43 +0200

    refactor(app):INIT-65 refactor app folder structure

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import text
+from . import db
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, unique=True, nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
+
+class Image(db.Model):
+    __tablename__ = "images"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False, index=True)
+    original_filename = db.Column(db.Text, nullable=False)
+    stored_filename = db.Column(db.Text, nullable=False, unique=True)
+    stored_path = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=text("now()"), nullable=False)
